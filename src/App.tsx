365904f6fed72d2b5619bef9a7f4b9f04ec72927
Bdmnss <lukabadali123@gmail.com>
2024-07-31 19:36:25 +0400

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Menu from "./components/Menu";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
   return (
-    <>
-      <Header />
-    </>
+    <div className="min-h-screen min-w-screen relative">
+      {/* Overlay */}
+      <div
+        className={`bg-black h-[100%] w-[100%] opacity-75 absolute ${
+          isMenuOpen ? "block" : "hidden"
+        }`}
+        onClick={() => {
+          setIsMenuOpen(false);
+          setIsOverlayOpen(false);
+        }}
+      ></div>
+      <Menu
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        setIsOverlayOpen={setIsOverlayOpen}
+      />
+      <Header
+        setIsMenuOpen={setIsMenuOpen}
+        setIsOverlayOpen={setIsOverlayOpen}
+      />
+    </div>
   );
 }
 
---
+++ src/components/Menu.tsx
@@ -0,0 +1,40 @@
+interface MenuProps {
+  isMenuOpen: boolean;
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsOverlayOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Menu: React.FC<MenuProps> = ({
+  isMenuOpen,
+  setIsMenuOpen,
+  setIsOverlayOpen,
+}) => {
+  return (
+    <nav
+      className={`bg-white h-[100%] w-[65%] z-10 absolute pl-[2.5rem] pt-[2.5rem] ${
+        isMenuOpen
+          ? "animate-slide-left-to-right"
+          : "animate-slide-right-to-left"
+      }`}
+    >
+      <div className="mb-[5.5rem]">
+        <img
+          src="./images/icon-close.svg"
+          alt="close icon"
+          onClick={() => {
+            setIsMenuOpen(false);
+            setIsOverlayOpen(false);
+          }}
+        />
+      </div>
+      <div className="flex flex-col gap-[2rem] text-[1.8rem] text-[#1d2026] font-bold">
+        <p>Collections</p>
+        <p>Men</p>
+        <p>Women</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+    </nav>
+  );
+};
+export default Menu;
